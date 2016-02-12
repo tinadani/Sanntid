b79853329c7f836696dec7cb6b118de12e7d8a0b
tinadani <tina.danielsen@outlook.com>
2016-02-12 14:09:11 +0100

    Update elevator.go

--- elevator.go
+++ elevator.go
@@ -1,137 +1,140 @@
 package driver 
 
-const MOTOR_SPEED = 2800
-const N_FLOORS int = 4
-const N_BUTTONS int = 3
+import (
+    "fmt"
+)
 
-button_type := map[string] int {
-	"Button call up" : 2,
-	"Button internal panel" : 1,
-	"Button call down" : 0,
+const MOTORSPEED = 2800
+const NUMFLOORS int = 4
+const NUMBUTTONS int = 3
+const ON bool = true
+const OFF bool = false
+var buttonType = map[string]int{
+	"Button call up": 0,
+	"Button call down": 1,
+    "Button internal panel": 2,
 }
 
- var lamp_channels_matrix = [N_FLOORS][N_BUTTON] int{
-    {LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
-    {LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
-    {LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
-    {LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4},
+ var lampChannelsMatrix = [NUMFLOORS][NUMBUTTONS] int{
+    {LIGHTUP1, LIGHTDOWN1, LIGHTCOMMAND1},
+    {LIGHTUP2, LIGHTDOWN2, LIGHTCOMMAND2},
+    {LIGHTUP3, LIGHTDOWN3, LIGHTCOMMAND3},
+    {LIGHTUP4, LIGHTDOWN4, LIGHTCOMMAND4},
  } 
 
- var button_channel_matrix = [N_FLOORS][N_BUTTONS] int{
-    {BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1},
-    {BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2},
-    {BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3},
-    {BUTTON_UP4, BUTTON_DOWN4, BUTTON_COMMAND4},
+ var buttonChannelsMatrix = [NUMFLOORS][NUMBUTTONS] int{
+    {BUTTONUP1, BUTTONDOWN1, BUTTONCOMMAND1},
+    {BUTTONUP2, BUTTONDOWN2, BUTTONCOMMAND2},
+    {BUTTONUP3, BUTTONDOWN3, BUTTONCOMMAND3},
+    {BUTTONUP4, BUTTONDOWN4, BUTTONCOMMAND4},
 }
 
-func Init_elevator() bool{
-	init_success bool = IO_init)()
-	if!init_success{
-		return False
-	}
 
-	for floor:=0; floor < N_FLOORS; floor++{
-		for int button = 0; button < N_BUTTONS; button++{
-			Set_button_lamp(button,floor,0)
-		}
-	}
-	Set_stop_lamp(0);
-    Set_door_open_lamp(0);
-    Set_floor_indicator(0);
-}
+func ElevatorSetMotorDirection(motorDirection int){
 
-func Elevator_set_motor_direction(motor_direction int){
-    
-    if motor_direction == 0{		
-        IO_write_analog(MOTOR, 0)
-    }
-    else if motor_direction > 0 { 	//Set direction up
-        IO_clear_bit(MOTORDIR)
-        IO_write_analog(MOTOR, MOTOR_SPEED)
-    } 
-    else if motor_direction < 0{ 		//Set direction down
-        IO_set_bit(MOTORDIR)
-        IO_write_analog(MOTOR, MOTOR_SPEED)
+    if motorDirection > 0 { 	//Set direction up if positive number
+        IOClearBit(MOTORDIR)
+        IOWriteAnalog(MOTOR, MOTORSPEED)
+    }else if motorDirection < 0 { 		//Set direction down if negative number
+        IOSetBit(MOTORDIR)
+        IOWriteAnalog(MOTOR, MOTORSPEED)
+    }else if (motorDirection == 0){        
+        IOWriteAnalog(MOTOR, 0)     //if not stop elevator
+    }else{
+        fmt.Println("Unable to set motor direction")
     }
 }
 
-func Elevator_set_button_lamp(button_type int, floor int, on bool){
+func ElevatorSetButtonLamp(buttonType int, floor int, on bool){
 
-	if floor =< 0 || floor > N_FLOORS{
-		fmt.Println('Invalid floor to set buttonlamp')
+	if (floor < 0) || (floor > NUMFLOORS){
+		fmt.Println("Invalid floor to set buttonlamp")
 	}
-	if 0 < button_type || button_type < N_BUTTONS {
-		fmt.Println('Invalid button type')
+
+    if (0 > buttonType) || (buttonType > NUMBUTTONS) {
+		fmt.Println("Invalid button type")
 	}
-	if on {
-		IO_set_bit(lamp_channels_matrix[floor][button_type])
-	}
-	else {
-		IO_clear_bit(lamp_channels_matrix[floor][button_type])
+
+    if on {
+		IOSetBit(lampChannelsMatrix[floor][buttonType])
+	}else {
+		IOClearBit(lampChannelsMatrix[floor][buttonType])
 	}
 }
 
-func Elevator_set_floor_indicator(floor int){
-	 if floor =< 0 || floor > N_FLOORS{
-		fmt.Println('Invalid floor to set floor indicator')
+func ElevatorSetFloorIndicator(floor int){
+	 if floor < 0 || floor > NUMFLOORS{
+		fmt.Println("Invalid floor to set floor indicator")
 	}
 
     // Binary encoding. One light must always be on.
-    if (floor & 0x02) {
-        io_set_bit(LIGHT_FLOOR_IND1);
+    if (floor & 0x02 != 0) {
+        IOSetBit(LIGHTFLOORIND1);
     } else {
-        io_clear_bit(LIGHT_FLOOR_IND1);
+        IOClearBit(LIGHTFLOORIND1);
     }    
 
-    if (floor & 0x01) {
-        io_set_bit(LIGHT_FLOOR_IND2);
+    if (floor & 0x01 != 0) {
+        IOSetBit(LIGHTFLOORIND2);
     } else {
-        io_clear_bit(LIGHT_FLOOR_IND2);
+        IOClearBit(LIGHTFLOORIND2);
     }   
 }
 
-func Elevator_set_door_open_lamp(value int) {
-    if (value) {
-        io_set_bit(LIGHT_DOOR_OPEN)
-    } 
-    else {
-        io_clear_bit(LIGHT_DOOR_OPEN)
+func ElevatorSetDoorOpenLamp(on bool) {
+    if (on) {
+        IOSetBit(LIGHTDOOROPEN)
+    }else {
+        IOClearBit(LIGHTDOOROPEN)
     }
 }
 
-func Elevator_get_button_signal(button_type int, floor int) int{
+func ElevatorGetButtonSignal(buttonType int, floor int) int{
 
-	if floor =< 0 || floor > N_FLOORS{
-		fmt.Println('Invalid floor to get button signal')
+	if (floor < 0 )|| (floor > NUMFLOORS){
+		fmt.Println("Invalid floor to get button signal")
 	}
-	if 0 < button_type || button_type < N_BUTTONS {
-		fmt.Println('Invalid button type to get button signal')
+	if (0 >= buttonType) || (buttonType > NUMBUTTONS ){
+		fmt.Println("Invalid button type to get button signal")
 	}
 
-
-    if io_read_bit(button_channels_matrix[floor][button_type]) {
+    if IOReadBit(buttonChannelsMatrix[floor][buttonType]) !=0 {
         return 1
-    }
-    else {
+    }else {
         return 0
     }    
 }
 
-func Elevator_get_floor_sensor_signal() int{
+func ElevatorGetFloorSensorSignal() int{
     
-    if IO_read_bit(SENSOR_FLOOR1) {
+    if IOReadBit(SENSORFLOOR1) != 0 {
         return 0;
-    } 
-    else if IO_read_bit(SENSOR_FLOOR2) {
+    }else if IOReadBit(SENSORFLOOR2) != 0 {
         return 1;
-    } 
-    else if IO_read_bit(SENSOR_FLOOR3) {
+    }else if IOReadBit(SENSORFLOOR3) != 0 { 
         return 2;
-    } 
-    else if IO_read_bit(SENSOR_FLOOR4) {
+    }else if IOReadBit(SENSORFLOOR4) != 0 {
         return 3;
-    } 
-    else {
+    }else {
         return -1;
     }
+}
+
+
+func InitializeElevator() bool{
+    var initSuccess bool = IOInitializeElevator()
+    
+    if !initSuccess {
+        return false
+    }
+
+    for floor := 0; floor < NUMFLOORS; floor++{
+        for button := 0; button < NUMBUTTONS; button++{
+            ElevatorSetButtonLamp(button,floor,OFF)
+        }
+    }
+
+    ElevatorSetDoorOpenLamp(OFF)
+    //ElevatorSetFloorIndicator(ElevatorGetFloorSensorSignal())
+    return true
 }
